504fa2f18c2b58ad27c865fc2d9cee5b4f747a44
Roberts Vesīks <alex36254@gmail.com>
2022-11-10 02:55:33 +0300

    added function for launcher

--- test.py
+++ test.py
@@ -3,15 +3,41 @@ from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
 
+import sys
+
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+def startgame():
+    window.close()
+    window2.show()
+
+# Подкласс QMainWindow для настройки главного окна приложения
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.l1=QPushButton("Let's Close this Window")
-        self.setCentralWidget(self.l1)
-        self.setWindowTitle('close window')
-        self.l1.clicked.connect(self.close)
 
-app=QApplication(sys.argv)
-window=MainWindow()
+        self.setWindowTitle("My App")
+        button = QPushButton("Press Me!")
+
+        # Устанавливаем центральный виджет Window.
+        self.setCentralWidget(button)
+
+        button.clicked.connect(startgame)
+
+class MainWindow2(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App 2")
+
+        # Устанавливаем центральный виджет Window.
+
+app = QApplication(sys.argv)
+
+window2= MainWindow2()
+window = MainWindow()
 window.show()
+
+
 app.exec()
