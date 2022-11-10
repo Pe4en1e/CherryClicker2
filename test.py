1fcf716d10f5bd1384b3dc3b698b78e66253e1ab
Roberts Vesīks <alex36254@gmail.com>
2022-11-10 05:40:02 +0300

    launcher core

--- test.py
+++ test.py
@@ -12,32 +12,101 @@ def startgame():
     window.close()
     window2.show()
 
-# Подкласс QMainWindow для настройки главного окна приложения
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
 
-        self.setWindowTitle("My App")
-        button = QPushButton("Press Me!")
+class Ui_Launcher(QMainWindow):
 
-        # Устанавливаем центральный виджет Window.
-        self.setCentralWidget(button)
 
-        button.clicked.connect(startgame)
+    def setupUi(self, Launcher):
+        Launcher.setObjectName("Launcher")
+        Launcher.resize(640, 320)
+        self.centralwidget = QWidget(Launcher)
+        self.centralwidget.setObjectName("centralwidget")
+        self.start_btn = QPushButton(self.centralwidget)
+        self.start_btn.setGeometry(QRect(340, 130, 141, 31))
+        self.start_btn.setObjectName("start_btn")
+        Launcher.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(Launcher)
+        self.menubar.setGeometry(QRect(0, 0, 640, 21))
+        self.menubar.setObjectName("menubar")
+        Launcher.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(Launcher)
+        self.statusbar.setObjectName("statusbar")
+        Launcher.setStatusBar(self.statusbar)
+        self.start_btn.clicked.connect(startgame)
 
-class MainWindow2(QMainWindow):
-    def __init__(self):
-        super().__init__()
+        self.retranslateUi(Launcher)
+        QMetaObject.connectSlotsByName(Launcher)
 
-        self.setWindowTitle("My App 2")
+    def retranslateUi(self, Launcher):
+        _translate = QCoreApplication.translate
+        Launcher.setWindowTitle(_translate("Launcher", "Launcher"))
+        self.start_btn.setText(_translate("Launcher", "PushButton"))
 
-        # Устанавливаем центральный виджет Window.
 
-app = QApplication(sys.argv)
+class Clicks():
+    total=0
+    click_cost=1
 
-window2= MainWindow2()
-window = MainWindow()
+
+class Ui_CherryClicker(object):
+    def setupUi(self, CherryClicker):
+        CherryClicker.setObjectName("CherryClicker")
+        CherryClicker.resize(1920, 1080)
+        self.centralwidget = QWidget(CherryClicker)
+        self.centralwidget.setObjectName("centralwidget")
+        self.main_btn = QPushButton(self.centralwidget)
+        self.main_btn.setGeometry(QRect(560, 610, 220, 90))
+        self.main_btn.setObjectName("main_btn")
+        self.score = QLabel(self.centralwidget)
+        self.score.setGeometry(QRect(170, 60, 391, 161))
+        font = QFont()
+        font.setPointSize(20)
+        self.score.setFont(font)
+        self.score.setObjectName("score")
+        CherryClicker.setCentralWidget(self.centralwidget)
+        self.statusbar = QStatusBar(CherryClicker)
+        self.statusbar.setObjectName("statusbar")
+        CherryClicker.setStatusBar(self.statusbar)
+
+        self.retranslateUi(CherryClicker)
+        QMetaObject.connectSlotsByName(CherryClicker)
+
+
+    def retranslateUi(self, CherryClicker):
+        _translate = QCoreApplication.translate
+        CherryClicker.setWindowTitle(_translate("CherryClicker", "CherryClicker"))
+        self.main_btn.setText(_translate("CherryClicker", "PushButton"))
+        self.score.setText(_translate("CherryClicker", str(Clicks.total)))
+        self.main_btn.clicked.connect(self.addscore)
+    
+
+    def addscore(self):
+        Clicks.total += Clicks.click_cost
+        self.score.setText(str(Clicks.total))
+
+
+
+
+app=QApplication(sys.argv)
+ui = Ui_Launcher()
+window=QMainWindow()
+window2=QMainWindow()
+gameUi=Ui_CherryClicker()
+gameUi.setupUi(window2)
+ui.setupUi(window)
 window.show()
-
-
 app.exec()
+
+
+    
+
+# if __name__ == "__main__":
+#     import sys
+#     app = QApplication(sys.argv)
+#     Launcher = QMainWindow()
+#     ui = Ui_Launcher()
+#     ui.setupUi(Launcher)
+#     Launcher.show()
+#     sys.exit(app.exec())
+
+
